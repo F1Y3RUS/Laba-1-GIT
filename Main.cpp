244df6cc2870c2520de7c83d943f79203dafdcbb
Anna <dzhagara@yandex.ru>
2018-11-18 22:27:39 +0300

    1.3.1

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include "std.h"
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+}
---
+++ std.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef STD_H
+#define STD_H
+int a, b; // Файл заголовка
+#endif
