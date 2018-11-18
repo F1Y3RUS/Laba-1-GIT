c43d6a21e399057b184b23142258cae5a7563b75
Anna <dzhagara@yandex.ru>
2018-11-18 22:45:03 +0300

    1.3.2

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << "¬ведите два целых числа" << endl;
+	cout << "¬ведите 2 целых числа" << endl;
 	cin >> a >> b;
 	_getch();
 	return 0;
